e6dcafa912c1d6b2e60d7fb9b1ff8f6df2b662cc
Zebulun Arendsee <arendsee@iastate.edu>
2017-10-11 17:13:27 -0500

    Successfully link to UnQLite on my system
    The code base is ANSI C and typedefs have changed. So the unqlite
    library needs to be loaded with `extern`.

--- src/io.cpp
+++ src/io.cpp
@@ -1,29 +1,33 @@
-// #include <unqlite.h>
+extern "C" {
+#include <unqlite.h>
+}
+#include <stdio.h>
 #include <Rcpp.h>
 
 // [[Rcpp::export]]
 Rcpp::List c_openDB(std::string dbfilename){
 
-    // unqlite *pDb;
-    //
-    // // Open our database;
-    // int rc = unqlite_open(&pDb, dbfilename, UNQLITE_OPEN_CREATE);
-    // if( rc != UNQLITE_OK ){
-    //     return;
-    // }
-    //
-    // Rcpp::XPtr<unqlite*> db = p(pDb, true);
-    //
-    // List out(2);
-    //
-    // out[1] = db;
-    // out[2] = rc;
+    unqlite *pDb;
+
+    // Open our database;
+    int rc = unqlite_open(&pDb, dbfilename.c_str(), UNQLITE_OPEN_CREATE);
+    if( rc != UNQLITE_OK ){
+        Rcpp::stop("Failed to open database");
+    }
+
+    Rcpp::XPtr<unqlite> db(pDb);
 
     Rcpp::List out(2);
 
-    out[0] = Rcpp::CharacterVector::create("foo");
-    out[1] = Rcpp::NumericVector::create(1,2);
+    out[0] = db;
+    out[1] = Rcpp::NumericVector::create(rc);
 
     return out;
 
 }
+
+
+// c_closeDB
+// c_unqlite_kv_store
+// c_unqlite_kv_delete
+// c_unqlite_kv_fetch
